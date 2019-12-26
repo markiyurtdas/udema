69c729a6230c96cec9729ea47773de68946a3f96
mukremin.bagci <mukremin.bagci@nucleo.com.tr>
2019-12-26 16:40:16 +0300

    second

---
+++ StartActivity/app/src/main/java/com/marki/startactivity/CourseInfoActivity.java
@@ -0,0 +1,14 @@
+package com.marki.startactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.course_info);
+    }
+}
